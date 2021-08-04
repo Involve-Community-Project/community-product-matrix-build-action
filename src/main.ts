088407d0a5789d44d1971fcf2e0310011f258486
Austin Dimmer <austin@effective-computing.com>
2021-08-04 23:34:22 +0200

    debugs locally and collects client infra stacks as output for build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,33 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github';
 import {wait} from './wait'
+import { env } from 'process';
+const path = require('path')
+var glob = require("glob")
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.debug(env.GITHUB_WORKSPACE!)
+    core.debug(env.PRODUCTION_STAGE!)
+    const cloudFormationTemplatesPath:string = path.join(env.GITHUB_WORKSPACE!, '/cdk.out/');
+    const patternToMatch: string = core.getInput('templateMatchPattern')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    var pattern = "@(" + env.PRODUCTION_STAGE! + "*InfraStack.template.json)"
+    console.log(pattern)
 
-    core.setOutput('time', new Date().toTimeString())
+
+    glob.Glob(cloudFormationTemplatesPath + pattern, {}, (err:any, files:any)=>{
+      core.debug(files)
+      core.setOutput('clientStacksToDeploy', files)
+    })
+    // const ms: string = core.getInput('milliseconds')
+    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+    // core.debug(new Date().toTimeString())
+    // await wait(parseInt(ms, 10))
+    // core.debug(new Date().toTimeString())
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
