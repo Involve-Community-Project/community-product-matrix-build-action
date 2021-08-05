218812d38c144f6cfc700573ab0b3f9c5b4a10ee
Austin Dimmer <austin@effective-computing.com>
2021-08-05 10:56:09 +0200

    trying out the test workflow on github

--- src/main.ts
+++ src/main.ts
@@ -5,32 +5,41 @@ import { env } from 'process';
 const path = require('path')
 var glob = require("glob")
 
-async function run(): Promise<void> {
+import { setFailed, info } from "@actions/core";
+import AWS from "aws-sdk";
+
+export interface Logger {
+  setFailed: (msg: any) => void;
+  info: (msg: any) => void;
+}
+
+async function run(logger: Logger): Promise<void> {
   try {
-    core.debug(env.GITHUB_WORKSPACE!)
-    core.debug(env.PRODUCTION_STAGE!)
+    logger.info("Github Workspace path is: " + env.GITHUB_WORKSPACE!)
+    logger.info("Production Stage is: " + env.PRODUCTION_STAGE!)
     const cloudFormationTemplatesPath:string = path.join(env.GITHUB_WORKSPACE!, '/cdk.out/');
     const patternToMatch: string = core.getInput('templateMatchPattern')
 
     var pattern = "@(" + env.PRODUCTION_STAGE! + "*InfraStack.template.json)"
-    console.log(pattern)
-
-
+    logger.info("Match Pattern is: " + pattern)
+    let filesToReturn: string[] = []
     glob.Glob(cloudFormationTemplatesPath + pattern, {}, (err:any, files:any)=>{
-      core.debug(files)
-      core.setOutput('clientStacksToDeploy', files)
+      logger.info(files)
+      filesToReturn = files
     })
-    // const ms: string = core.getInput('milliseconds')
-    // core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    core.setOutput('matrix', filesToReturn)
 
-    // core.debug(new Date().toTimeString())
-    // await wait(parseInt(ms, 10))
-    // core.debug(new Date().toTimeString())
+    const ms: string = core.getInput('milliseconds')
+    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
-    // core.setOutput('time', new Date().toTimeString())
+    core.debug(new Date().toTimeString())
+    await wait(parseInt(ms, 10))
+    core.debug(new Date().toTimeString())
+
+    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
-    core.setFailed(error.message)
+    logger.setFailed(error.message)
   }
 }
 
-run()
+run({ setFailed, info })
